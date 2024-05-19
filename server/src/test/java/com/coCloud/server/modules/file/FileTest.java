86af12d534016c8f2e0f129abf9276eb6805ef59
Agility6 <agility1013@gmail.com>
2024-05-20 00:23:57 +0800

    feat: test file mould

--- server/src/test/java/com/coCloud/server/modules/file/FileTest.java
+++ server/src/test/java/com/coCloud/server/modules/file/FileTest.java
@@ -1,15 +1,21 @@
 package com.coCloud.server.modules.file;
 
 import cn.hutool.core.lang.Assert;
+import com.coCloud.core.exception.CoCloudBusinessException;
+import com.coCloud.core.utils.IdUtil;
 import com.coCloud.server.CoCloudServerLauncher;
-import com.coCloud.server.modules.file.context.QueryFileListContext;
+import com.coCloud.server.modules.file.context.*;
+import com.coCloud.server.modules.file.entity.CoCloudFile;
 import com.coCloud.server.modules.file.enums.DelFlagEnum;
+import com.coCloud.server.modules.file.service.IFileService;
 import com.coCloud.server.modules.file.service.IUserFileService;
 import com.coCloud.server.modules.file.vo.CoCloudUserFileVO;
 import com.coCloud.server.modules.user.context.UserLoginContext;
 import com.coCloud.server.modules.user.context.UserRegisterContext;
 import com.coCloud.server.modules.user.service.IUserService;
 import com.coCloud.server.modules.user.vo.UserInfoVO;
+import com.google.common.collect.Lists;
+import lombok.val;
 import org.apache.commons.collections.CollectionUtils;
 import org.junit.After;
 import org.junit.Test;
@@ -17,7 +23,9 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -30,6 +38,7 @@ import java.util.List;
  */
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest(classes = CoCloudServerLauncher.class)
+@Transactional
 public class FileTest {
 
     @Autowired
@@ -37,6 +46,9 @@ public class FileTest {
 
     @Autowired
     private IUserService iUserService;
+
+    @Autowired
+    private IFileService iFileService;
 
     /**
      * 测试用户查询文件列表成功
@@ -54,6 +66,290 @@ public class FileTest {
         context.setDelFlag(DelFlagEnum.NO.getCode());
         List<CoCloudUserFileVO> result = iUserFileService.getFileList(context);
         Assert.isTrue(CollectionUtils.isEmpty(result));
+    }
+
+    /**
+     * 测试创建文件夹成功
+     */
+    @Test
+    public void testCreateFolderSuccess() {
+        Long userId = register();
+        UserInfoVO userInfoVO = info(userId);
+
+        CreateFolderContext context = new CreateFolderContext();
+        context.setParentId(userInfoVO.getRootFileId());
+        context.setUserId(userId);
+        context.setFolderName("folder-name");
+
+        Long fileId = iUserFileService.createFolder(context);
+        Assert.notNull(fileId);
+    }
+
+    /**
+     * 测试文件重命名失败-文件ID无效
+     */
+    @Test(expected = CoCloudBusinessException.class)
+    public void testUpdateFilenameFailByWrongFileId() {
+        Long userId = register();
+        UserInfoVO userInfoVO = info(userId);
+
+        CreateFolderContext context = new CreateFolderContext();
+        context.setParentId(userInfoVO.getRootFileId());
+        context.setUserId(userId);
+        context.setFolderName("folder-name");
+
+        Long fileId = iUserFileService.createFolder(context);
+        Assert.notNull(fileId);
+
+        UpdateFilenameContext updateFilenameContext = new UpdateFilenameContext();
+        updateFilenameContext.setFileId(fileId + 1);
+        updateFilenameContext.setUserId(userId);
+        updateFilenameContext.setNewFilename("folder-name-new");
+
+        iUserFileService.updateFilename(updateFilenameContext);
+    }
+
+    /**
+     * 测试当前用户ID无效
+     */
+    @Test(expected = CoCloudBusinessException.class)
+    public void testUpdateFilenameFailByWrongUserId() {
+        Long userId = register();
+        UserInfoVO userInfoVO = info(userId);
+
+        CreateFolderContext context = new CreateFolderContext();
+        context.setParentId(userInfoVO.getRootFileId());
+        context.setUserId(userId);
+        context.setFolderName("folder-name");
+
+        Long fileId = iUserFileService.createFolder(context);
+        Assert.notNull(fileId);
+
+        UpdateFilenameContext updateFilenameContext = new UpdateFilenameContext();
+        updateFilenameContext.setFileId(fileId);
+        updateFilenameContext.setUserId(userId + 1);
+        updateFilenameContext.setNewFilename("folder-name-new");
+
+        iUserFileService.updateFilename(updateFilenameContext);
+    }
+
+    /**
+     * 测试文件名称重复
+     */
+    @Test(expected = CoCloudBusinessException.class)
+    public void testUpdateFilenameFailByWrongFilename() {
+        Long userId = register();
+        UserInfoVO userInfoVO = info(userId);
+
+        CreateFolderContext context = new CreateFolderContext();
+        context.setParentId(userInfoVO.getRootFileId());
+        context.setUserId(userId);
+        context.setFolderName("folder-name");
+
+        Long fileId = iUserFileService.createFolder(context);
+        Assert.notNull(fileId);
+
+        UpdateFilenameContext updateFilenameContext = new UpdateFilenameContext();
+        updateFilenameContext.setFileId(fileId);
+        updateFilenameContext.setUserId(userId);
+        updateFilenameContext.setNewFilename("folder-name");
+
+        iUserFileService.updateFilename(updateFilenameContext);
+    }
+
+    /**
+     * 校验文件名称已被占用
+     */
+    @Test(expected = CoCloudBusinessException.class)
+    public void testUpdateFilenameFailByFilenameUnAvailable() {
+        Long userId = register();
+        UserInfoVO userInfoVO = info(userId);
+
+        CreateFolderContext context = new CreateFolderContext();
+        context.setParentId(userInfoVO.getRootFileId());
+        context.setUserId(userId);
+        context.setFolderName("folder-name-1");
+
+        Long fileId = iUserFileService.createFolder(context);
+        Assert.notNull(fileId);
+
+        context.setParentId(userInfoVO.getRootFileId());
+        context.setUserId(userId);
+        context.setFolderName("folder-name-2");
+
+        fileId = iUserFileService.createFolder(context);
+        Assert.notNull(fileId);
+
+        UpdateFilenameContext updateFilenameContext = new UpdateFilenameContext();
+        updateFilenameContext.setFileId(fileId);
+        updateFilenameContext.setUserId(userId);
+        updateFilenameContext.setNewFilename("folder-name-1");
+
+        iUserFileService.updateFilename(updateFilenameContext);
+    }
+
+    /**
+     * 测试更新文件名称成功
+     */
+    @Test
+    public void testUpdateFilenameSuccess() {
+        Long userId = register();
+        UserInfoVO userInfoVO = info(userId);
+
+        CreateFolderContext context = new CreateFolderContext();
+        context.setParentId(userInfoVO.getRootFileId());
+        context.setUserId(userId);
+        context.setFolderName("folder-name-old");
+
+        Long fileId = iUserFileService.createFolder(context);
+        Assert.notNull(fileId);
+
+        UpdateFilenameContext updateFilenameContext = new UpdateFilenameContext();
+        updateFilenameContext.setFileId(fileId);
+        updateFilenameContext.setUserId(userId);
+        updateFilenameContext.setNewFilename("folder-name-new");
+
+        iUserFileService.updateFilename(updateFilenameContext);
+
+    }
+
+//    /**
+//     * 校验文件删除失败-非法的文件ID
+//     */
+//    @Test(expected = CoCloudBusinessException.class)
+//    public void testDeleteFileFailByWrongFileId() {
+//        Long userId = register();
+//        UserInfoVO userInfoVO = info(userId);
+//
+//        CreateFolderContext context = new CreateFolderContext();
+//        context.setParentId(userInfoVO.getRootFileId());
+//        context.setUserId(userId);
+//        context.setFolderName("folder-name-old");
+//
+//        Long fileId = iUserFileService.createFolder(context);
+//        Assert.notNull(fileId);
+//
+//        DeleteFileContext deleteFileContext = new DeleteFileContext();
+//        List<Long> fileIdList = Lists.newArrayList();
+//        fileIdList.add(fileId + 1);
+//        deleteFileContext.setFileIdList(fileIdList);
+//        deleteFileContext.setUserId(userId);
+//
+//        iUserFileService.deleteFile(deleteFileContext);
+//    }
+//
+//    /**
+//     * 校验文件删除失败-非法的用户ID
+//     */
+//    @Test(expected = CoCloudBusinessException.class)
+//    public void testDeleteFileFailByWrongUserId() {
+//        Long userId = register();
+//        UserInfoVO userInfoVO = info(userId);
+//
+//        CreateFolderContext context = new CreateFolderContext();
+//        context.setParentId(userInfoVO.getRootFileId());
+//        context.setUserId(userId);
+//        context.setFolderName("folder-name-old");
+//
+//        Long fileId = iUserFileService.createFolder(context);
+//        Assert.notNull(fileId);
+//
+//        DeleteFileContext deleteFileContext = new DeleteFileContext();
+//        List<Long> fileIdList = Lists.newArrayList();
+//        fileIdList.add(fileId);
+//        deleteFileContext.setFileIdList(fileIdList);
+//        deleteFileContext.setUserId(userId + 1);
+//
+//        iUserFileService.deleteFile(deleteFileContext);
+//    }
+//
+//    /**
+//     * 校验用户删除文件成功
+//     */
+//    @Test
+//    public void testDeleteFileSuccess() {
+//        Long userId = register();
+//        UserInfoVO userInfoVO = info(userId);
+//
+//        CreateFolderContext context = new CreateFolderContext();
+//        context.setParentId(userInfoVO.getRootFileId());
+//        context.setUserId(userId);
+//        context.setFolderName("folder-name-old");
+//
+//        Long fileId = iUserFileService.createFolder(context);
+//        Assert.notNull(fileId);
+//
+//        DeleteFileContext deleteFileContext = new DeleteFileContext();
+//        List<Long> fileIdList = Lists.newArrayList();
+//        fileIdList.add(fileId);
+//        deleteFileContext.setFileIdList(fileIdList);
+//        deleteFileContext.setUserId(userId);
+//
+//        iUserFileService.deleteFile(deleteFileContext);
+//    }
+
+    /**
+     * 校验秒传文件成功
+     */
+    @Test
+    public void testSecUploadSuccess() {
+        Long userId = register();
+        UserInfoVO userInfoVO = info(userId);
+
+        String identifier = "123456789";
+
+        CoCloudFile record = new CoCloudFile();
+        record.setFileId(IdUtil.get());
+        record.setFilename("filename");
+        record.setRealPath("realpath");
+        record.setFileSize("fileSize");
+        record.setFileSizeDesc("fileSizeDesc");
+        record.setFilePreviewContentType("");
+        record.setIdentifier(identifier);
+        record.setCreateUser(userId);
+        record.setCreateTime(new Date());
+        iFileService.save(record);
+
+        SecUploadFileContext context = new SecUploadFileContext();
+        context.setIdentifier(identifier);
+        context.setFilename("filename");
+        context.setParentId(userInfoVO.getRootFileId());
+        context.setUserId(userId);
+
+        boolean result = iUserFileService.secUpload(context);
+        Assert.isTrue(result);
+    }
+
+    /**
+     * 校验秒传文件失败
+     */
+    @Test
+    public void testSecUploadFail() {
+        Long userId = register();
+        UserInfoVO userInfoVO = info(userId);
+
+        String identifier = "123456789";
+
+        CoCloudFile record = new CoCloudFile();
+        record.setFileId(IdUtil.get());
+        record.setFilename("filename");
+        record.setRealPath("realpath");
+        record.setFileSize("fileSize");
+        record.setFileSizeDesc("fileSizeDesc");
+        record.setFilePreviewContentType("");
+        record.setIdentifier(identifier);
+        record.setCreateUser(userId);
+        record.setCreateTime(new Date());
+        iFileService.save(record);
+
+        SecUploadFileContext context = new SecUploadFileContext();
+        context.setIdentifier(identifier + "_update");
+        context.setFilename("filename");
+        context.setParentId(userInfoVO.getRootFileId());
+        context.setUserId(userId);
+
+        boolean result = iUserFileService.secUpload(context);
+        Assert.isFalse(result);
     }
 
     /* =============> private <============= */
